7339b166687280a70bfe330698a7311f5f39a589
Robin TURPIN <robin.turpin@campus.academy>
2021-08-09 18:45:48 +0200

    Downloading some bundles, started coding

--- tap-and-go/src/components/App.js
+++ tap-and-go/src/components/App.js
@@ -1,15 +1,11 @@
 import logo from '../logo.svg';
 import '../App.css';
 import Banner from './Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Banner />
-      </header>
-    </div>
   );
 }
 
--- tap-and-go/src/components/Banner.js
+++ tap-and-go/src/components/Banner.js
@@ -1,5 +1,24 @@
+import '../styles/Banner.css';
+import logo from '../assets/logo.png';
+
 function Banner() {
-    return <h1>TapAndG<span>&#128692;</span></h1>
+    return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light shadow p-2 mb-2 rounded'>
+        <div className='container-fluid'>
+            <a href="/"><img src={logo} alt='Tap And Go' className='tag-logo' /></a>
+            <div className='collapse navbar-collapse' id="navbarNav">
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <a href='/' className='nav-link'>Carte</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='/' className='nav-link'>Liste stations</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    )
 }
 
 export default Banner;
